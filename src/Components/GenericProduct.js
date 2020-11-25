d06b4db520e0780261a1d19da68fa32461224e99
Pooja Nangude <poojanangude@Poojas-MacBook-Air.local>
2020-11-25 14:43:31 +0530

    onclick image

--- src/Components/GenericProduct.js
+++ src/Components/GenericProduct.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './App.css';
 import '../index.css';
 
-const GenericProduct =({image, name, price},props) =>{
+const handleSubmit = (id) =>{
+  alert("handle submit"+id);
+}
+
+const GenericProduct =({id,image, name, price}) =>{
     let height="300px";
     let width="300px";
   return (
-  <article className='product'>
+  <article className='product' onClick={()=>handleSubmit(id)}>
     <img src={image} height={height} width={width} alt="" />
     <div className="App">
     <h3>{name}</h3>
