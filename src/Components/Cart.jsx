c713f3ea67f1b1fa5216e154d6c6d8dfe0a67432
Pooja Nangude <poojanangude@Poojas-MacBook-Air.local>
2020-11-28 20:05:01 +0530

    cart functionality

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -4,70 +4,81 @@ import products from '../Constants/products';
 import ListGroup from 'react-bootstrap/ListGroup';
 import { Container, Col, Row, Button } from "react-bootstrap";
 import Image from 'react-bootstrap/Image';
+import { Redirect } from "react-router-dom";
 
 const Cart = (props) => {
   const [total,setTotal]=useState(0);
-  let id=1;
+  let id = props.loggedIn.userid;
   let sum=0;
-  let m = users.find((x) => x.id === id);
-  console.log(m.cart);
-  let arr=m.cart;
 
-  {products.map((product) => {
-       if(arr.includes(product.id)){
-         sum=sum+product.price;
-       }
-  })}
+  if (id === 0) {
+    return <Redirect to="/login"></Redirect>;
+  } else {
+    let user = users.filter((user) => user.id === id);
 
-  return (
-    <div>
-            <div>
+    let items = [];
+    for (let item of user[0].cart) {
+      items.push(
+        products.filter((prod) => prod.id.toString() === item.toString())
+      );
+    }
+
+    for(let rec of items){
+      sum=sum+rec[0].price;
+      console.log(sum);
+    }
+
+
+    return (
+      <div>
+        <h1>Your Cart</h1>
         <ListGroup>
-        {products.map((product) => {
-          if(arr.includes(product.id)){
-            return (
-              <div key={product.id}>
+        {items.map((item) => {
+          return (
+            <div  key={item[0].id}>
                 <ListGroup.Item>
                   <Container>
                     <Row>
                       <Col md={3}>
                         <Row>
-                          <Image src={product.image} height="150px" width="150px" rounded />
+                          <Image src={item[0].image} height="150px" width="150px" rounded />
                         </Row>
                       </Col>
 
                       <Col md={5}>
                         <Row>
-                          <h5>{product.name}</h5>
+                          <h5>{item[0].name}</h5>
                         </Row>
                       </Col>
 
                       <Col>
                         <Row>
-                          <h6>${product.price}</h6>
+                          <h6>${item[0].price}</h6>
+                          
                         </Row>
                       </Col>
 
                     </Row>
                   </Container>
                 </ListGroup.Item>
-              </div>
-            );
-          } 
+            </div>
+          );
         })}
         </ListGroup>
         <Row>
           <Col md={6}>
-            <h2>Total : {sum}</h2>
+            <h1>
+              Total : {sum}
+            </h1>
           </Col>
-        <Col>
-        <Button>Proceed to Purchase All</Button>
-        </Col>
+          <Col md={6}>
+            <Button>Proceed to Buy All</Button>
+          </Col>
         </Row>
+
       </div>
-    </div>
-  )
+    );
+  }
 }
 
 export default Cart
-
