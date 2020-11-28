506a9d712198e97e1d18c0ca12b79cefcf862e34
Pooja Nangude <poojanangude@Poojas-MacBook-Air.local>
2020-11-28 20:08:10 +0530

    Changes in routes

--- src/Components/Routes.jsx
+++ src/Components/Routes.jsx
@@ -7,19 +7,25 @@ import Login from "../Common/Login";
 import Category from "./Category";
 import Products from "./Products";
 import Logout from "../Common/Logout";
-import AddToCart from './AddToCart';
+import Purchase from "./Purchase";
 
 const Routes = (props) => {
   return (
     <Switch>
       <Route exact path="/category/books">
-        <Category category="books"></Category>
+        <Category category="books" loggedIn={props.loggedIn}></Category>
       </Route>
       <Route exact path="/category/mobiles">
-        <Category category="phone"></Category>
+        <Category category="phone" loggedIn={props.loggedIn}></Category>
       </Route>
-      <Route exact path="/wishlist" component={Wishlist}></Route>
-      <Route exact path="/cart" component={Cart}></Route>
+      <Route exact path="/wishlist">
+        <Wishlist loggedIn={props.loggedIn}></Wishlist>
+      </Route>
+      <Route exact path="/cart">
+        <Cart loggedIn={props.loggedIn}></Cart>
+      </Route>
+
+      {/* <Route exact path="/cart" component={Cart}></Route> */}
       <Route exact path="/login">
         <Login
           loggedIn={props.loggedIn}
@@ -27,23 +33,17 @@ const Routes = (props) => {
         ></Login>
       </Route>
       <Route exact path="/logout">
-        <Logout loggedIn={props.loggedIn} 
-        setLoggedIn={props.setLoggedIn}
-        ></Logout>
-      </Route>
-
-      {/* <Route exact path="/addtocart">
-        <AddToCart
+        <Logout
           loggedIn={props.loggedIn}
           setLoggedIn={props.setLoggedIn}
-          ></AddToCart>
-            {console.log(props.loggedIn)}
-
-      </Route> */}
-
+        ></Logout>
+      </Route>
       {/* <Route path="/products" component={Products}></Route> */}
-      <Route exact path="/products/:id" component={Products}></Route>
-      <Route exact path="/" component={Home}></Route>
+      <Route exact path="/products/:id/:userid" component={Products} /> ;
+      <Route exact path="/purchase/:id" component={Purchase} /> ;
+      <Route exact path="/">
+        <Home loggedIn={props.loggedIn}></Home>
+      </Route>
     </Switch>
   );
 };
