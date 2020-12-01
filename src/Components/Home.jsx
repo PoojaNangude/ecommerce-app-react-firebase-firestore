4735afbe20bfcb6da03fcbbc23a8cd0dec299644
Pooja Nangude <poojanangude@Poojas-MacBook-Air.local>
2020-12-01 09:37:52 +0530

    updates

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 import Carousel from "./Carousel";
-import RandomProducts from "./RandomProducts";
 import "./App.css";
+import products from '../Constants/products';
+import ProductCards from './ProductCards';
+
+const RandomProducts = (props) => {
+  return (
+    <div>
+      <h1 className="App">Some of our Products</h1>
+      <div className="productlist">
+        {products.slice(0, 6).map((product) => {
+          return (
+            <ProductCards
+              key={product.id}
+              loggedIn={props.loggedIn}
+              {...product}
+            ></ProductCards>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export const Home = (props) => {
   return (
